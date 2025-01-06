fb09a9c4d500baca56612a239d8793753fcafceb
Jiyoung Kim <jyblue.kim@gmail.com>
2025-01-06 23:59:18 +0900

    Update start_redis_server.sh

--- redis-6.2.7/test_cluster/redis-master/start_redis_server.sh
+++ redis-6.2.7/test_cluster/redis-master/start_redis_server.sh
@@ -44,3 +44,62 @@ if [ ! -d "$TARGET_DIR" ]; then
 else
     echo "Diriectory $TARGET_DIR already exists. No action needed."
 fi
+
+SOURCE_FILE="./redis-6378.conf"
+DESTINATION_FILE="/etc/redis/redis-6378.conf"
+echo "Step 04: Copy and overwrite $TARGET_DIR ..."
+if [ ! -f "$SOURCE_FILE" ]; then
+    echo "Source file $SOURCE_FILE does not exist. Aborting setup script."
+    exit 1
+cp -f "$TARGET_FILE" "$DESTINATION_FILE"
+if [ $? -eq 0 ]; then
+    echo "File successfully copied from $SOURCE_FILE to $DESTINATION_FILE."
+else
+    echo "Failed to copy file. Aborting setup script."
+fi
+
+SOURCE_FILE="./redis-server-6378.service"
+DESTINATION_FILE="/etc/systemd/system/redis-server-6378.conf"
+echo "Step 05: Copy and overwrite $TARGET_DIR ..."
+if [ ! -f "$SOURCE_FILE" ]; then
+    echo "Source file $SOURCE_FILE does not exist. Aborting setup script."
+    exit 1
+cp -f "$TARGET_FILE" "$DESTINATION_FILE"
+if [ $? -eq 0 ]; then
+    echo "File successfully copied from $SOURCE_FILE to $DESTINATION_FILE."
+else
+    echo "Failed to copy file. Aborting setup script."
+fi
+
+SERVICE_NAME="redis-server-6378"
+systemctl stop $SERVICE_NAME
+echo "Step 06: Stop service $SERVICE_NAME ..."
+if [ $? -eq 0 ]; then
+    echo "Service $SERVICE_NAME successfully stopped."
+else
+    echo "Failed to stop service $SERVICE_NAME. Aborting setup script."
+fi
+
+systemctl daemon-reload
+echo "Step 07: systemd daemon reloading ..."
+if [ $? -eq 0 ]; then
+    echo "systemd daemon successfully reloaded."
+else
+    echo "Failed to reload systemd daemon. Aborting setup script."
+fi
+
+systemctl enable $SERVICE_NAME
+echo "Step 08: Enabling service $SERVICE_NAME ..."
+if [ $? -eq 0 ]; then
+    echo "Service $SERVICE_NAME successfully enabled."
+else
+    echo "Failed to enable service $SERVICE_NAME. Aborting setup script."
+fi
+
+systemctl start $SERVICE_NAME
+echo "Step 09: Starting service $SERVICE_NAME ..."
+if [ $? -eq 0 ]; then
+    echo "Service $SERVICE_NAME successfully started.."
+else
+    echo "Failed to start service $SERVICE_NAME. Aborting setup script."
+fi
