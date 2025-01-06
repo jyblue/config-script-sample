8e25184a744850252dbc643d140d8269fc4b1a09
Jiyoung Kim <jyblue.kim@gmail.com>
2025-01-06 23:38:28 +0900

    Update start_redis_server.sh

--- redis-6.2.7/test_cluster/redis-master/start_redis_server.sh
+++ redis-6.2.7/test_cluster/redis-master/start_redis_server.sh
@@ -4,7 +4,7 @@ TARGET_DIR="/etc/redis"
 echo "Step 01: Checking if directory $TARGET_DIR exists..."
 if [ ! -d "$TARGET_DIR" ]; then
     echo "Diriectory $TARGET_DIR does not exist. Proceeding to create the directory.
-    make -p "$TARGET_DIR"
+    mkdir -p "$TARGET_DIR"
     if [ $? -eq 0 ]; then
         echo "Diriectory $TARGET_DIT successfully created."
     else
@@ -19,7 +19,7 @@ TARGET_DIR="/var/log/redis"
 echo "Step 02: Checking if directory $TARGET_DIR exists..."
 if [ ! -d "$TARGET_DIR" ]; then
     echo "Diriectory $TARGET_DIR does not exist. Proceeding to create the directory.
-    make -p "$TARGET_DIR"
+    mkdir -p "$TARGET_DIR"
     if [ $? -eq 0 ]; then
         echo "Diriectory $TARGET_DIT successfully created."
     else
@@ -34,7 +34,7 @@ TARGET_DIR="/var/lib/redis"
 echo "Step 03: Checking if directory $TARGET_DIR exists..."
 if [ ! -d "$TARGET_DIR" ]; then
     echo "Diriectory $TARGET_DIR does not exist. Proceeding to create the directory.
-    make -p "$TARGET_DIR"
+    mkdir -p "$TARGET_DIR"
     if [ $? -eq 0 ]; then
         echo "Diriectory $TARGET_DIT successfully created."
     else
