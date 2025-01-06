312c43e3780042a25752eb9f417062a5f1a8e661
Jiyoung Kim <jyblue.kim@gmail.com>
2025-01-06 23:36:44 +0900

    Create start_redis_server.sh

---
+++ redis-6.2.7/test_cluster/redis-master/start_redis_server.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+TARGET_DIR="/etc/redis"
+echo "Step 01: Checking if directory $TARGET_DIR exists..."
+if [ ! -d "$TARGET_DIR" ]; then
+    echo "Diriectory $TARGET_DIR does not exist. Proceeding to create the directory.
+    make -p "$TARGET_DIR"
+    if [ $? -eq 0 ]; then
+        echo "Diriectory $TARGET_DIT successfully created."
+    else
+        echo "Failed to create directory $TARGET_DIR. Aborting setup script."
+        exit 1
+    fi
+else
+    echo "Diriectory $TARGET_DIR already exists. No action needed."
+fi
+
+TARGET_DIR="/var/log/redis"
+echo "Step 02: Checking if directory $TARGET_DIR exists..."
+if [ ! -d "$TARGET_DIR" ]; then
+    echo "Diriectory $TARGET_DIR does not exist. Proceeding to create the directory.
+    make -p "$TARGET_DIR"
+    if [ $? -eq 0 ]; then
+        echo "Diriectory $TARGET_DIT successfully created."
+    else
+        echo "Failed to create directory $TARGET_DIR. Aborting setup script."
+        exit 1
+    fi
+else
+    echo "Diriectory $TARGET_DIR already exists. No action needed."
+fi
+
+TARGET_DIR="/var/lib/redis"
+echo "Step 03: Checking if directory $TARGET_DIR exists..."
+if [ ! -d "$TARGET_DIR" ]; then
+    echo "Diriectory $TARGET_DIR does not exist. Proceeding to create the directory.
+    make -p "$TARGET_DIR"
+    if [ $? -eq 0 ]; then
+        echo "Diriectory $TARGET_DIT successfully created."
+    else
+        echo "Failed to create directory $TARGET_DIR. Aborting setup script."
+        exit 1
+    fi
+else
+    echo "Diriectory $TARGET_DIR already exists. No action needed."
+fi
